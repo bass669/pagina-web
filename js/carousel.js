eb7ee27a89ba8b892ecc54481232e4900a3e3c42
S0ul <141199149+elpanxitoo@users.noreply.github.com>
2026-03-26 01:52:34 -0300

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,3 +1,38 @@
-$('input').on('change', function() {
-  $('body').toggleClass('blue');
+document.addEventListener("DOMContentLoaded", function() {
+    let index = 1;
+    const totalItems = 3;
+    let autoPlay = true;
+
+    // Función que cambia el slide
+    const changeSlide = () => {
+        if (autoPlay) {
+            index++;
+            if (index > totalItems) index = 1;
+            
+            const nextRadio = document.getElementById(`item-${index}`);
+            if (nextRadio) {
+                nextRadio.checked = true;
+                console.log("cambio")
+            }
+        }
+    };
+
+    // Crear el intervalo (10 segundos)
+    let carruselInterval = setInterval(changeSlide, 5000);
+    const sliderContainer = document.getElementById('quienessomos');
+
+    if (sliderContainer) {
+        // Pausar con el mouse entra
+        sliderContainer.addEventListener('mouseenter', () => {
+            autoPlay = false;
+            console.log("Carrusel pausado"); 
+        });
+        
+        // Reanudar con el mouse sale
+        sliderContainer.addEventListener('mouseleave', () => {
+            autoPlay = true;
+            console.log("Carrusel reanudado");
+        });
+
+    }
 });
