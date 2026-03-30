380459b7a73bc205a3a70e9bdf5dc4683776c758
S0ul <141199149+elpanxitoo@users.noreply.github.com>
2026-03-30 00:46:12 -0300

    Add files via upload

---
+++ js/formulario.js
@@ -0,0 +1,44 @@
+ (function(){
+        emailjs.init("ke3vfKzM3kDH6SA4L");
+    })();
+
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        const btn = event.target.querySelector('button');
+        const textoOriginal = btn.innerText;
+        btn.innerText = 'Enviando...';
+        btn.disabled = true; // Evita múltiples envíos
+
+        emailjs.sendForm('service_lf9j5mr', 'template_2stcwyb', this)
+            .then(function() {
+                // VENTANA EMERGENTE DE ÉXITO
+                Swal.fire({
+                    title: '¡Mensaje Enviado!',
+                    text: 'Te contactaremos a la brevedad.',
+                    icon: 'success',
+                    background: '#1a1a1a', 
+                    color: '#fff',
+                    showConfirmButton: false, // Oculta el botón OK
+                    timer: 3000, // SE CIERRA EN 3 SEGUNDOS
+                    timerProgressBar: true, // Muestra la barrita de tiempo
+                    iconColor: '#54e600' 
+                });
+
+                btn.innerText = textoOriginal;
+                btn.disabled = false;
+                event.target.reset();
+            }, function(error) {
+                // VENTANA DE ERROR
+                Swal.fire({
+                    title: 'Error',
+                    text: 'Hubo un problema al enviar: ' + JSON.stringify(error),
+                    icon: 'error',
+                    background: '#1a1a1a',
+                    color: '#fff',
+                    confirmButtonColor: '#e60000'
+                });
+                btn.innerText = textoOriginal;
+                btn.disabled = false;
+            });
+    });
